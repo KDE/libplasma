Sonic-DE/sonic-interface-libraries

Use correct prefix SVG prefix for tooltips
In various porting using the correct prefix for tooltips got lost.

This patch restores that by adding an additional prefix argument to the
constructor of PlasmaWindow and having a separate DialogShadows instance
per SVG prefix.

BUG: 415601

src/plasmaquick/dialogshadows_p.h
@@ -17,10 +17,10 @@ class DialogShadows : public KSvg::Svg
     Q_OBJECT
 
 public:
-    explicit DialogShadows(QObject *parent = nullptr, const QString &prefix = QStringLiteral("dialogs/background"));
-    ~DialogShadows() override;
+    static DialogShadows *instance(const QString &prefix = QStringLiteral("dialogs/background"));
 
-    static DialogShadows *self();
+    explicit DialogShadows(QObject *parent, const QString &prefix);
+    ~DialogShadows() override;
 
     void addWindow(QWindow *window, KSvg::FrameSvg::EnabledBorders enabledBorders = KSvg::FrameSvg::AllBorders);
     void removeWindow(QWindow *window);

KDE/libplasma

src/plasmaquick/dialogshadows_p.h
@@ -17,10 +17,10 @@ class DialogShadows : public KSvg::Svg
     Q_OBJECT
 
 public:
-    explicit DialogShadows(QObject *parent = nullptr, const QString &prefix = QStringLiteral("dialogs/background"));
-    ~DialogShadows() override;
+    static DialogShadows *instance(const QString &prefix = QLatin1String("dialogs/background"));
 
-    static DialogShadows *self();
+    explicit DialogShadows(QObject *parent, const QString &prefix);
+    ~DialogShadows() override;
 
     void addWindow(QWindow *window, KSvg::FrameSvg::EnabledBorders enabledBorders = KSvg::FrameSvg::AllBorders);
     void removeWindow(QWindow *window);
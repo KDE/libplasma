2f8c6355be284a588e93f0a91c3a2f16692b09e9
Marco Martin <notmart@gmail.com>
2024-05-29 11:20:34 +0200

    Fix error message
    Don't try to do version checks if the package doesn't exists at all
    If the applet is not found on disk just output
    
    Error loading Applet: package does not exist

--- src/plasmaquick/appletquickitem.cpp
+++ src/plasmaquick/appletquickitem.cpp
@@ -525,45 +525,46 @@ AppletQuickItem *AppletQuickItem::itemForApplet(Plasma::Applet *applet)
         errorData[QStringLiteral("appletName")] = i18n("Unknown Applet");
         errorData[QStringLiteral("isDebugMode")] = qEnvironmentVariableIntValue("PLASMA_ENABLE_QML_DEBUG") != 0;
 
-        const QString versionString = applet->pluginMetaData().value(QStringLiteral("X-Plasma-API-Minimum-Version"));
-        QVersionNumber version;
-        if (!versionString.isEmpty()) {
-            version = QVersionNumber::fromString(versionString);
-        }
-
-        bool versionMismatch = false;
-        const int plasma_version_major = 6; // TODO: as soon PLASMA_VERSION_MAJOR is actually 6, use directly that
-        if (version.isNull()) {
-            reason = i18n(
-                "This Widget was written for an unknown older version of Plasma and is not compatible with Plasma %1. Please contact the widget's author for "
-                "an updated version.",
-                plasma_version_major);
-            compactReason = i18n("%1 is not compatible with Plasma %2", applet->pluginMetaData().name(), plasma_version_major);
-            versionMismatch = true;
-        } else if (version.majorVersion() < plasma_version_major) {
-            reason =
-                i18n("This Widget was written for Plasma %1 and is not compatible with Plasma %2. Please contact the widget's author for an updated version.",
-                     version.majorVersion(),
-                     plasma_version_major);
-            compactReason = i18n("%1 is not compatible with Plasma %2", applet->pluginMetaData().name(), plasma_version_major);
-            versionMismatch = true;
-        } else if (version.majorVersion() > plasma_version_major || version.minorVersion() > PLASMA_VERSION_MINOR) {
-            reason = i18n("This Widget was written for Plasma %1 and is not compatible with Plasma %2. Please update Plasma in order to use the widget.",
-                          versionString,
-                          plasma_version_major);
-            compactReason = i18n("%1 is not compatible with Plasma %2", applet->pluginMetaData().name(), plasma_version_major);
-            versionMismatch = true;
-        } else if (applet->failedToLaunch()) {
-            reason = applet->launchErrorMessage();
-            compactReason = reason;
-        } else {
-            compactReason = i18n("Sorry! There was an error loading %1.", applet->pluginMetaData().name());
-        }
-        errorData[QStringLiteral("errors")] = QJsonArray::fromStringList({reason});
-        if (compactReason != QString()) {
-            errorData[QStringLiteral("compactError")] = compactReason;
-        }
         if (applet->sourceValid()) {
+            const QString versionString = applet->pluginMetaData().value(QStringLiteral("X-Plasma-API-Minimum-Version"));
+            QVersionNumber version;
+            if (!versionString.isEmpty()) {
+                version = QVersionNumber::fromString(versionString);
+            }
+
+            bool versionMismatch = false;
+            const int plasma_version_major = 6; // TODO: as soon PLASMA_VERSION_MAJOR is actually 6, use directly that
+            if (version.isNull()) {
+                reason = i18n(
+                    "This Widget was written for an unknown older version of Plasma and is not compatible with Plasma %1. Please contact the widget's author for "
+                    "an updated version.",
+                    plasma_version_major);
+                compactReason = i18n("%1 is not compatible with Plasma %2", applet->pluginMetaData().name(), plasma_version_major);
+                versionMismatch = true;
+            } else if (version.majorVersion() < plasma_version_major) {
+                reason =
+                    i18n("This Widget was written for Plasma %1 and is not compatible with Plasma %2. Please contact the widget's author for an updated version.",
+                        version.majorVersion(),
+                        plasma_version_major);
+                compactReason = i18n("%1 is not compatible with Plasma %2", applet->pluginMetaData().name(), plasma_version_major);
+                versionMismatch = true;
+            } else if (version.majorVersion() > plasma_version_major || version.minorVersion() > PLASMA_VERSION_MINOR) {
+                reason = i18n("This Widget was written for Plasma %1 and is not compatible with Plasma %2. Please update Plasma in order to use the widget.",
+                            versionString,
+                            plasma_version_major);
+                compactReason = i18n("%1 is not compatible with Plasma %2", applet->pluginMetaData().name(), plasma_version_major);
+                versionMismatch = true;
+            } else if (applet->failedToLaunch()) {
+                reason = applet->launchErrorMessage();
+                compactReason = reason;
+            } else {
+                compactReason = i18n("Sorry! There was an error loading %1.", applet->pluginMetaData().name());
+            }
+            errorData[QStringLiteral("errors")] = QJsonArray::fromStringList({reason});
+            if (compactReason != QString()) {
+                errorData[QStringLiteral("compactError")] = compactReason;
+            }
+
             if (!versionMismatch) {
                 const auto errors = qmlObject->mainComponent()->errors();
                 QStringList errorList;
@@ -576,7 +577,6 @@ AppletQuickItem *AppletQuickItem::itemForApplet(Plasma::Applet *applet)
             errorData[QStringLiteral("appletName")] = applet->pluginMetaData().name();
             reason += i18n("Error loading QML file: %1 %2", qmlObject->mainComponent()->url().toString(), reason);
         } else {
-            // TODO: here also try to detect if the package was a P5 one
             reason += i18n("Error loading Applet: package does not exist. %1", applet->launchErrorMessage());
             errorData[QStringLiteral("errors")] = QJsonArray::fromStringList({reason});
         }

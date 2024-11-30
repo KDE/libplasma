4c1a1bb44fb65036b7918b63a8ee92abfdb95f6c
l10n daemon script <scripty@kde.org>
2024-11-30 03:05:55 +0000

    SVN_SILENT made messages (.desktop file) - always resolve ours
    In case of conflict in i18n, keep the version of the branch "ours"
    To resolve a particular conflict, "git checkout --ours path/to/file.desktop"

--- src/desktoptheme/oxygen/metadata.json.cmake
+++ src/desktoptheme/oxygen/metadata.json.cmake
@@ -4,6 +4,7 @@
             {
                 "Email": "kde-artists@kde.org",
                 "Name": "The Oxygen Project",
+                "Name[ar]": "مشروع أكسجين",
                 "Name[az]": "Oxygen layihəsi",
                 "Name[be]": "Праект \"Oxygen\"",
                 "Name[bg]": "Проектът Oxygen",
@@ -52,6 +53,7 @@
         ],
         "Category": "",
         "Description": "Theme done in the Oxygen style",
+        "Description[ar]": "سمة بواسطة نمط أوكسجين",
         "Description[az]": "Mövzu Oxygen stilində hazırlanıb",
         "Description[be]": "Тэма створаная ў стылі Oxygen",
         "Description[bg]": "Тема в стил Oxygen",

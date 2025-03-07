8b0dbd82c7a69893371626b57806edecba1ae31a
Nate Graham <nate@kde.org>
2025-03-07 15:36:04 +0000

    ContainmentItem: delete wallpaper item immediately to fix crash
    When ContainmentItem receives ItemSceneChange, the window might
    be destroyed soon. However, if deleteLater is used, the window might be
    destroyed before the wallpaper item, but the wallpaper item still
    accesses the dangling window in its dtor, which causes a crash.
    
    BUG: 499944
    SENTRY: PLASMA-WORKSPACE-1Y5
    FIXED-IN: 6.3.3
    
    
    (cherry picked from commit 027b9180038ddfddf712a8123da54bfc223a8691)
    
    Co-authored-by: Fushan Wen <qydwhotmail@gmail.com>

--- src/plasmaquick/plasmoid/containmentitem.cpp
+++ src/plasmaquick/plasmoid/containmentitem.cpp
@@ -1087,7 +1087,7 @@ void ContainmentItem::itemChange(ItemChange change, const ItemChangeData &value)
 void ContainmentItem::deleteWallpaperItem()
 {
     m_containment->setProperty("wallpaperGraphicsObject", QVariant());
-    m_wallpaperItem->deleteLater();
+    delete m_wallpaperItem; // SENTRY: PLASMA-WORKSPACE-1Y5
     m_wallpaperItem = nullptr;
 }
 

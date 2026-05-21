f57fecda34b93b4778422b09621e4341a45278d2
Nicolas Fella <nicolas.fella@gmx.de>
2026-05-21 20:55:49 +0200

    Expose Plasma's shared QML engine more directly
    In Plasma we want to use one QML engine for most things (applets, OSDs, etc).
    
    This is implemented in SharedQmlEngine. It's a lot of scaffolding around what
    is effectively a QQmlEngine singleton. Most of it doesn't add much on top
    of QQmlEngine functionality.
    
    Expose the engine as a public API so that components can use it without
    having to go through SharedQmlEngine, and port SharedQmlEngine to use it.
    
    This will eventually allow to remove SharedQmlEngine entirely

---
+++ src/plasmaquick/plasmaquick.h
@@ -0,0 +1,20 @@
+/*
+ *  SPDX-FileCopyrightText: 2026 Nicolas Fella <nicolas.fella@gmx.de>
+ *
+ *  SPDX-License-Identifier: LGPL-2.0-or-later
+ */
+
+#pragma once
+
+#include "plasmaquick_export.h"
+
+#include <QQmlEngine>
+
+namespace PlasmaQuick
+{
+/*!
+ * The shared QML engine to be used for applets and other Plasma components.
+ * \since 6.8
+ */
+PLASMAQUICK_EXPORT std::shared_ptr<QQmlEngine> globalEngine();
+};
